pchaigno/cilium

commit 688dc9ac802b11f6c16a9cbc5d60baaf77bd6ed0

ipsec: Remove stale XFRM states and policies
We recently changed our XFRM states and policies (IPs and marks). We
however failed to remove the stale XFRM states and policies and it turns
out that they conflict (e.g., the kernel ends up picking the stale
policies for encryption instead of the new one).

This commit therefore cleans up those stale XFRM states and policies. We
can identify them based on mark values and masks (we switched from
0xFF00 to 0XFFFFFF00).

The new XFRM states and policies are added as we receive the information
on remote nodes. By removing the stale states and policies before the
new ones are installed for all nodes, we could cause plain-text traffic
on egress and packet drops on ingress.

To ensure we never let plain-text traffic out, we will clean up the
stale config only once the catch-all default-drop policy is installed.
In that way, if there is a brief moment where, for a connection
nodeA -> nodeB, we don't have a policy, traffic will be dropped instead
of sent in plain-text.

For each connection nodeA -> nodeB, those packet drops on egress and
ingress of nodeA will happen between the time we replace the BPF
datapath and the time we've installed the new XFRM state and policy
corresponding to nodeB. Waiting longer to remove the stale states and
policies doesn't impact the drops as they will keep happening until the
new states and policies are installed. This is all happening on agent
startup, as soon as we have the necessary information from k8s.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -115,6 +116,10 @@ var (
 		Action:   netlink.XFRM_POLICY_BLOCK,
 		Priority: 1,
 	}
+
+	// To attempt to remove any stale XFRM configs once at startup, after
+	// we've added the catch-all default-drop policy.
+	removeStaleXFRMOnce sync.Once
 )
 
 func getIPSecKeys(ip net.IP) *ipSecKey {
@@ -372,12 +377,102 @@ func IpSecReplacePolicyFwd(dst *net.IPNet, tmplDst net.IP) error {
 //
 // We do need to match on the mark because there is also traffic flowing
 // through XFRM that we don't want to encrypt (e.g., hostns traffic).
-func IPsecDefaultDropPolicy(ipv6 bool) error {
+func IPsecDefaultDropPolicy(ipv6 bool) (err error) {
 	defaultDropPolicy := defaultDropPolicyIPv4
+	family := netlink.FAMILY_V4
 	if ipv6 {
 		defaultDropPolicy = defaultDropPolicyIPv6
+		family = netlink.FAMILY_V6
 	}
+
+	// We call removeStaleStatesAndPolicies only if the catch-all default-drop
+	// policy was successfully installed. If it was not installed, then there's
+	// a danger of letting plain-text traffic leave the node if we remove stale
+	// XFRM configs.
+	// This code can be removed in Cilium v1.15.
+	defer func() {
+		if err == nil {
+			removeStaleStatesAndPolicies(family)
+		}
+	}()
+
 	return netlink.XfrmPolicyUpdate(defaultDropPolicy)
+}
+
+// Removes XFRM states and policies that are identified as installed by a
+// previous version of Cilium. We rely mainly on the mark mask to identify if
+// it was installed in a previous version. These states and policies need to be
+// removed for cross-node connectivity to work.
+func removeStaleStatesAndPolicies(family int) {
+	removeStaleXFRMOnce.Do(func() {
+		removeStalePolicies(family)
+		removeStaleStates(family)
+	})
+}
+
+func removeStalePolicies(family int) {
+	policies, err := netlink.XfrmPolicyList(family)
+	if err != nil {
+		log.WithError(err).Error("Cannot get XFRM policies")
+	}
+	for _, p := range policies {
+		switch p.Dir {
+		case netlink.XFRM_DIR_OUT:
+			if isDefaultDropPolicy(&p) {
+				continue
+			}
+			if p.Mark.Mask != linux_defaults.IPsecOldMarkMaskOut {
+				// This XFRM OUT policy was not installed by a previous version of Cilium.
+				continue
+			}
+		case netlink.XFRM_DIR_IN:
+			if p.Src.String() != wildcardCIDRv4.String() ||
+				p.Mark.Mask != linux_defaults.IPsecMarkMaskIn {
+				// This XFRM IN policy was not installed by a previous version of Cilium.
+				continue
+			}
+		default:
+			continue
+		}
+		if err := netlink.XfrmPolicyDel(&p); err != nil {
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.SourceCIDR:      p.Src,
+				logfields.DestinationCIDR: p.Dst,
+			}).Error("Failed to remove stale XFRM policy")
+		}
+	}
+}
+
+func removeStaleStates(family int) {
+	states, err := netlink.XfrmStateList(family)
+	if err != nil {
+		log.WithError(err).Error("Cannot get XFRM states")
+	}
+	for _, s := range states {
+		scopedLog := log.WithFields(logrus.Fields{
+			logfields.SPI:           s.Spi,
+			logfields.SourceIP:      s.Src,
+			logfields.DestinationIP: s.Dst,
+		})
+
+		if s.Mark.Mask == linux_defaults.IPsecOldMarkMaskOut &&
+			s.Mark.Value == ipSecXfrmMarkSetSPI(linux_defaults.RouteMarkEncrypt, uint8(s.Spi)) {
+			// This XFRM state was installed by Cilium.
+			if err := netlink.XfrmStateDel(&s); err == nil {
+				scopedLog.Info("Removed stale XFRM OUT state")
+			} else {
+				scopedLog.WithError(err).Error("Failed to remove stale XFRM OUT state")
+			}
+		} else if s.Mark.Mask == linux_defaults.IPsecMarkMaskIn &&
+			s.Mark.Value == linux_defaults.RouteMarkDecrypt {
+			// This XFRM state was installed by Cilium.
+			if err := netlink.XfrmStateDel(&s); err == nil {
+				scopedLog.Info("Removed stale XFRM IN state")
+			} else {
+				scopedLog.WithError(err).Error("Failed to remove stale XFRM IN state")
+			}
+		}
+	}
 }
 
 // ipSecXfrmMarkSetSPI takes a XfrmMark base value, an SPI, returns the mark

pkg/datapath/linux/linux_defaults/linux_defaults.go
@@ -106,8 +106,12 @@ const (
 	// IPsecMarkMaskNodeID is the mask used for the node ID.
 	IPsecMarkMaskNodeID = 0xFFFF0000
 
+	// IPsecOldMarkMaskOut is the mask that was previously used. It can be
+	// removed in Cilium v1.15.
+	IPsecOldMarkMaskOut = 0xFF00
+
 	// IPsecMarkMask is the mask required for the IPsec SPI, node ID, and encrypt/decrypt bits
-	IPsecMarkMaskOut = 0xFF00 | IPsecMarkMaskNodeID
+	IPsecMarkMaskOut = IPsecOldMarkMaskOut | IPsecMarkMaskNodeID
 
 	// IPsecMarkMaskIn is the mask required for IPsec to lookup encrypt/decrypt bits
 	IPsecMarkMaskIn = 0x0F00

pkg/logging/logfields/logfields.go
@@ -632,6 +632,8 @@ const (
 
 	DestinationIP = "destinationIP"
 
+	SourceCIDR = "sourceCIDR"
+
 	// DestinationCIDR is a destination CIDR
 	DestinationCIDR = "destinationCIDR"
 

commit 7e23509cb88de68d2fed96c81551fbfb20ea3426

ipsec: Don't remove stale XFRM IN configs
The XFRM IN policies and states didn't change so we should never need
to remove any stale XFRM IN configs. Let's thus simplify the logic to
find stale policies and states accordingly.

Fixes: 688dc9ac80 ("ipsec: Remove stale XFRM states and policies")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -426,11 +426,8 @@ func removeStalePolicies(family int) {
 				continue
 			}
 		case netlink.XFRM_DIR_IN:
-			if p.Src.String() != wildcardCIDRv4.String() ||
-				p.Mark.Mask != linux_defaults.IPsecMarkMaskIn {
-				// This XFRM IN policy was not installed by a previous version of Cilium.
-				continue
-			}
+			// The XFRM IN policies didn't change so we don't want to remove them.
+			continue
 		default:
 			continue
 		}
@@ -457,19 +454,11 @@ func removeStaleStates(family int) {
 
 		if s.Mark.Mask == linux_defaults.IPsecOldMarkMaskOut &&
 			s.Mark.Value == ipSecXfrmMarkSetSPI(linux_defaults.RouteMarkEncrypt, uint8(s.Spi)) {
-			// This XFRM state was installed by Cilium.
+			// This XFRM state was installed by a previous version of Cilium.
 			if err := netlink.XfrmStateDel(&s); err == nil {
 				scopedLog.Info("Removed stale XFRM OUT state")
 			} else {
 				scopedLog.WithError(err).Error("Failed to remove stale XFRM OUT state")
-			}
-		} else if s.Mark.Mask == linux_defaults.IPsecMarkMaskIn &&
-			s.Mark.Value == linux_defaults.RouteMarkDecrypt {
-			// This XFRM state was installed by Cilium.
-			if err := netlink.XfrmStateDel(&s); err == nil {
-				scopedLog.Info("Removed stale XFRM IN state")
-			} else {
-				scopedLog.WithError(err).Error("Failed to remove stale XFRM IN state")
 			}
 		}
 	}